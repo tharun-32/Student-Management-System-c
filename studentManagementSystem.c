e13276f98cea3cdd4e3d399822038205917f8a17
tharun <tharun.e.dev@gmail.com>
2026-02-20 13:08:15 +0530

    added functions to read and add data

--- studentManagementSystem.c
+++ studentManagementSystem.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 typedef struct{
     int id;
@@ -8,15 +9,51 @@ typedef struct{
     double cgpa;
 }Student ;
 
+void add();
+void delete();
+void show();
+
 int main(){
+    char user_choice;
+    char buffer[4];
+    do{
+        printf("Studen Management System\n");
+        printf("\n");
+        printf("1.Add student\n");
+        printf("2.Remove student\n");
+        printf("3.View record\n");
+        printf("4.exit\n");
+        printf("Enter your choice: ");
+        fgets(buffer,sizeof(buffer),stdin);
+        user_choice= (int)strtol(buffer,NULL,10);
+        printf("\n");
+        printf("\033[H\033[J");//clear screen for next loop
+        switch (user_choice) {
+            case 1:
+                add();
+            case 2:
+                delete();
+            case 3:
+                show();
+        }
+    }while(user_choice != 4);
+    return 0;
+}
+
+//functions 
+//add
+void add(){
     char buffer[50];
-    printf("Studen Management System\n");
     int no_of_students;
     printf("Total number of students: ");
     fgets(buffer,sizeof(buffer),stdin);
     no_of_students = (int)strtol(buffer,NULL,10);
     Student *student = malloc(no_of_students * sizeof(Student));
-    FILE *pFile = fopen("student_data.txt","w");
+    if(student == NULL){
+        printf("Memory allocation failed!\n");
+        return;
+    }
+    FILE *pFile = fopen("student_data.txt","a");
     for(int i=0; i < no_of_students; i++){
         fprintf(pFile,"student: %d\n",i+1);
         printf("student: %d\n",i+1);
@@ -36,9 +73,22 @@ int main(){
         fprintf(pFile,"id: %d\n",student[i].id);
         fprintf(pFile,"name: %s\n",student[i].name);
         fprintf(pFile,"cgpa: %.2lf\n",student[i].cgpa);
+        fprintf(pFile,"\n"); // while printing there will be a gap 
     };
     fclose(pFile);
     free(student);
     student = NULL;
-    return 0;
+}
+//remove
+void delete(){
+
+}
+//view
+void show(){
+    FILE *pFile = fopen("student_data.txt","r");
+    char buffer[1024] = {0};
+    while(fgets(buffer,sizeof(buffer),pFile) != NULL){
+        printf("%s",buffer);
+    }
+    fclose(pFile);
 }
